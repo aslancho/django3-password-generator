034cf2c40f4427ff5c47a8fb5a65019fbdf47180
Aslan Dadayev <aslan_dadaev@bk.ru>
2022-04-14 21:34:40 +0600

    I made some tweaks

--- pass_gen/settings.py
+++ pass_gen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(jms7q305^gqdqwy1(7ba&h##d_r2r=13(3rmezr4(==^$=dgy'
+SECRET_KEY = 'django-insecure-(jms7q305^gqdqwy1(7ba45&h##d_r2r=13(3rmezr4(==^$=dgy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
